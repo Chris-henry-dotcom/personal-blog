36f76a83ddf62396cc977033dd7e59062fe046c6
chris <user@Chriss-laptop.local>
2025-02-14 22:00:32 +0200

    finished home page view and post models deails

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-pub_date')
     return render(request, 'posts/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
